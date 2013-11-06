122f07c83e6f27fcfe5c3cbf95c1da2727f8d596
Owain Perry <owain.perry@thetrainline.com>
2013-11-06 17:04:37 +0000

    Added sonar file

--- src/Application/Properties/AssemblyInfo.cs
+++ src/Application/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProductAttribute("Product Name 1.0.0.0")]
 [assembly: AssemblyCopyrightAttribute(" (C) Copyright Company Name 2013")]
 [assembly: AssemblyVersionAttribute("1.0.0.0")]
-[assembly: AssemblyInformationalVersionAttribute("1.0.0.0 / 1326e92")]
+[assembly: AssemblyInformationalVersionAttribute("1.0.0.0 / ac6ce6d")]
 [assembly: AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: AssemblyDelaySignAttribute(false)]
 
